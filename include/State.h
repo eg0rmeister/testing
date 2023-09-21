5aa3771f658b709ce93a2b85764f46e45c37cf52
dm1trykrylov <231014d@gmail.com>
2023-09-22 00:14:17 +0300

    Add unique IDs for states

--- include/State.h
+++ include/State.h
@@ -3,25 +3,30 @@
 
 #include <string>
 
-enum{
-  IDENT,
-  LPARENT,
-  RPARENT,
-  COMMON
-};
+enum { IDENT, LPARENT, RPARENT, COMMON };
 
 class State {
  public:
-  State(const std::string& label, bool is_final,
-        const uint8_t type = COMMON);
+  State(const std::string& label, bool is_final, const uint8_t type = COMMON);
   bool IsFinal() const;
   std::string Label() const;
   uint8_t Type() const;
+  uint32_t ID() const;
 
  private:
   std::string _label;
   uint8_t _type;  // 'common' - for non-final, other - for final states
   bool _is_final;
+  uint32_t _id;
+  static uint32_t _global_id;
+};
+
+bool operator==(const State& lhs, const State& rhs);
+
+// Instantiate Hash for State to store State objects in unordered sets and maps
+template <>
+struct std::hash<State> {
+  std::size_t operator()(const State& state) const;
 };
 
 #endif
--- src/State.cpp
+++ src/State.cpp
@@ -1,10 +1,25 @@
 #include "State.h"
 
+uint32_t State::_global_id = 0;
+
 State::State(const std::string& label, bool is_final, uint8_t type)
-    : _label(label), _is_final(is_final), _type(type) {}
+    : _label(label), _is_final(is_final), _type(type), _id(++_global_id) {}
 
 bool State::IsFinal() const { return this->_is_final; }
 
 std::string State::Label() const { return this->_label; }
 
 uint8_t State::Type() const { return this->_type; }
+
+uint32_t State::ID() const { return this->_id; }
+
+bool operator==(const State& lhs, const State& rhs) {
+  return lhs.ID() == rhs.ID();
+}
+
+// Instantiate Hash for State to store State objects in unordered sets and maps
+
+std::size_t std::hash<State>::operator()(const State& state) const {
+  // Use unique state ID
+  return state.ID();
+}
