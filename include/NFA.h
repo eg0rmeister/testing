06deacd8bec13710133ab1fb691933acc2cb9d2d
dm1trykrylov <231014d@gmail.com>
2023-09-23 01:23:54 +0300

    Update NFA, move some methods to FSA

--- include/NFA.h
+++ include/NFA.h
@@ -5,22 +5,15 @@
 
 #include <FSA.h>
 
-#include <vector>
 
 class NFA : public FSA {
  public:
-  NFA(const State& start_state, const states_set& states,
+  NFA(const State& start_state, const states_vec& states,
       const transitions_map& transitions);
 
   // void SetStartState(const State& new_start_state);
 
-  std::vector<State> GetFinalStates();
-
   State& GetState(const State& state);
-
-  void MakeFinal(const State& state);
-
-  void MakeNonFinal(const State& state);
 
  private:
 };
--- src/NFA.cpp
+++ src/NFA.cpp
@@ -1,7 +1,5 @@
 #include <NFA.h>
 
-NFA::NFA(const State& start_state, const states_set& states,
-      const transitions_map& transitions)
-    : FSA(start_state, states, transitions) {}
-
-
+NFA::NFA(const State& start_state, const states_vec& states,
+         const transitions_map& transitions)
+    : FSA(start_state, states) {}
