7c4afdad003bb72485b1af5cffe46b52d7e48ad1
dm1trykrylov <231014d@gmail.com>
2023-09-23 01:22:42 +0300

    Rewrite FSA, now built on pointers

--- include/FSA.h
+++ include/FSA.h
@@ -8,22 +8,32 @@
 
 #include <unordered_map>
 #include <unordered_set>
+#include <vector>
 
 class FSA {
  public:
-  using states_set = std::unordered_set<State>;
-  using transitions_set = std::unordered_set<Transition>;
-  using transitions_map = std::unordered_map<State, transitions_set>;
-  FSA(const State& start_state, const states_set& states,
-      const transitions_map& transitions);
+  using state_ptr = State*;
+  using states_vec = std::vector<State>;
+  using states_set = std::vector<state_ptr>;
+  using states_map = std::unordered_map<uint32_t, state_ptr>;
+  using final_states_map = std::unordered_map<uint32_t, bool>;
+  using transitions_set = std::vector<Transition>;
+  using transitions_map = std::unordered_map<uint32_t, transitions_set>;
+  FSA(const State& start_state, const states_vec& states);
 
   void Reset();
-  void AddTransition(const State& fromState, const Transition& transition);
+  void AddTransition(uint32_t from_id, const Transition& transition);
+  state_ptr GetState(uint32_t state_id);
+  std::vector<State> GetFinalStates();
+  void SetFinal(uint32_t state_id, bool is_final = true);
 
  private:
-  State _start_state;
-  State _current_state;
+  state_ptr _start_state;
+  state_ptr _current_state;
+  state_ptr _trash;
   states_set _states;
+  states_map _state_by_id;
+  final_states_map _is_final_state;
   transitions_map _transitions;
 };
 
---
+++ src/FSA.cpp
@@ -0,0 +1,38 @@
+#include "FSA.h"
+
+FSA::FSA(const State& start_state, const states_vec& states)
+    : _start_state(new State(start_state)),
+      _current_state(new State(start_state)),
+      _trash(new State("T", true, TRASH)) {
+  for (auto& state : states) {
+    _states.push_back(new State(state));
+    _state_by_id[state.ID()] = *_states.rbegin();
+    _is_final_state[state.ID()] = false;
+  }
+}
+
+void FSA::Reset() { _current_state = _start_state; }
+
+void FSA::AddTransition(uint32_t from_id, const Transition& transition) {
+  _transitions[from_id].push_back(transition);
+}
+
+FSA::state_ptr FSA::GetState(uint32_t state_id) {
+  return _state_by_id[state_id];
+}
+
+std::vector<State> FSA::GetFinalStates() {
+  std::vector<State> final_states;
+  for (auto state : _states) {
+    if (_is_final_state[state->ID()]) {
+      final_states.emplace_back(*state);
+    }
+  }
+  return final_states;
+}
+
+void FSA::SetFinal(uint32_t state_id, bool is_final) {
+  if (_is_final_state.contains(state_id)) {
+    _is_final_state[state_id] = is_final;
+  }
+}
