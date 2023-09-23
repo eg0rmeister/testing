17c86361b0711ad73bb1191ec6fb9a758150167a
dm1trykrylov <231014d@gmail.com>
2023-09-23 19:08:51 +0300

    Add docs and some NFA builders

--- src/NFA.cpp
+++ src/NFA.cpp
@@ -2,3 +2,34 @@
 
 NFA::NFA(const State& start_state, const states_vec& states)
     : FSA(start_state, states) {}
+
+NFA BuildEmptyNFA()
+{
+  auto start_state = State("s", false);
+  auto end_state = State("f", true);
+  std::vector<State>states({start_state, end_state});
+  auto nfa = NFA(start_state, states);
+  nfa.SetFinal(end_state.ID());
+  return nfa;
+}
+
+NFA BuildEpsNFA()
+{
+  auto start_state = State("s", false);
+  std::vector<State>states({start_state});
+  auto nfa = NFA(start_state, states);
+  nfa.SetFinal(start_state.ID());
+  return nfa;
+}
+
+NFA BuildOneLetterNFA(std::string letter)
+{
+  auto start_state = State("s", false);
+  auto end_state = State("f", true);
+  std::vector<State>states({start_state, end_state});
+  auto nfa = NFA(start_state, states);
+  auto transition = Transition(letter, nfa.GetState(end_state.ID()));
+  nfa.AddTransition(start_state.ID(), transition);
+  nfa.SetFinal(end_state.ID());
+  return nfa;
+}
