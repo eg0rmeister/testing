562e11105f9325b6667d41bd990e3a6071af44a6
eg0rmeister <karpuhin.egor@gmail.com>
2023-09-24 18:45:19 +0300

    defined MergeNFS and added tests to it

--- src/NFA.cpp
+++ src/NFA.cpp
@@ -3,30 +3,55 @@
 NFA::NFA(const State& start_state, const states_vec& states)
     : FSA(start_state, states) {}
 
-NFA BuildEmptyNFA()
-{
+NFA MergeNFA(const NFA& lhs, const NFA& rhs) {
+  NFA::states_vec states;
+  NFA::states_vec left_states = lhs.GetStates();
+  NFA::states_vec right_states = rhs.GetStates();
+  states.reserve(left_states.size() + right_states.size());
+  for (auto state : left_states) {
+    states.push_back(state);
+  }
+  for (auto state : right_states) {
+    states.push_back(state);
+  }
+  NFA ret(lhs.GetStartState(), states);
+  for (auto transitions_source : lhs.GetAllTransitions()) {
+    for (auto transition : transitions_source.second)
+      ret.AddTransition(transitions_source.first, transition);
+  }
+  for (auto transitions_source : rhs.GetAllTransitions()) {
+    for (auto transition : transitions_source.second)
+      ret.AddTransition(transitions_source.first, transition);
+  }
+  for (auto final_state : rhs.GetFinalStates()) {
+    ret.SetFinal(final_state.ID(), true);
+  }
+  ret.AddTransition(
+      lhs.GetFinalStates()[0].ID(),
+      Transition("", ret.GetState(rhs.GetStartState().ID())));
+  return ret;
+}
+NFA BuildEmptyNFA() {
   auto start_state = State("s", false);
   auto end_state = State("f", true);
-  std::vector<State>states({start_state, end_state});
+  std::vector<State> states({start_state, end_state});
   auto nfa = NFA(start_state, states);
   nfa.SetFinal(end_state.ID());
   return nfa;
 }
 
-NFA BuildEpsNFA()
-{
+NFA BuildEpsNFA() {
   auto start_state = State("s", false);
-  std::vector<State>states({start_state});
+  std::vector<State> states({start_state});
   auto nfa = NFA(start_state, states);
   nfa.SetFinal(start_state.ID());
   return nfa;
 }
 
-NFA BuildOneLetterNFA(std::string letter)
-{
+NFA BuildOneLetterNFA(std::string letter) {
   auto start_state = State("s", false);
   auto end_state = State("f", true);
-  std::vector<State>states({start_state, end_state});
+  std::vector<State> states({start_state, end_state});
   auto nfa = NFA(start_state, states);
   auto transition = Transition(letter, nfa.GetState(end_state.ID()));
   nfa.AddTransition(start_state.ID(), transition);
---
+++ tests/test_Merge.cpp
@@ -0,0 +1,74 @@
+#include <NFA.h>
+#include <State.h>
+#include <Transition.h>
+
+#include <cassert>
+#include <iostream>
+#include <string>
+#include <unordered_set>
+#include <vector>
+
+int main() {
+  std::string label1 = "left_start";
+  std::string label2 = "left_end";
+  std::string label3 = "right_start";
+  std::string label4 = "right_middle";
+  std::string label5 = "right_end";
+
+  try {
+    State state1(label1, false);
+    State state2(label2, true);
+    State state3(label3, false);
+    State state4(label4, false);
+    State state5(label5, true);
+
+    std::vector<State> left_states;
+    left_states.push_back(state1);
+    left_states.push_back(state2);
+    
+    std::vector<State> right_states;
+    right_states.push_back(state3);
+    right_states.push_back(state4);
+    right_states.push_back(state5);
+
+    NFA left_nfa(state1, left_states);
+    NFA right_nfa(state3, right_states);
+    
+    auto state2_ptr = left_nfa.GetState(state2.ID());
+    auto state3_ptr = left_nfa.GetState(state3.ID());
+    auto state4_ptr = left_nfa.GetState(state4.ID());
+    auto state5_ptr = left_nfa.GetState(state5.ID());
+    auto transition12 = Transition("a", state2_ptr);
+    auto transition34 = Transition("b", state4_ptr);
+    auto transition35 = Transition("c", state5_ptr);
+    auto transition45 = Transition("d", state5_ptr);
+    auto transition43 = Transition("e", state5_ptr);
+    left_nfa.AddTransition(state1.ID(), transition12);
+    right_nfa.AddTransition(state3.ID(), transition34);
+    right_nfa.AddTransition(state3.ID(), transition35);
+    right_nfa.AddTransition(state4.ID(), transition45);
+    right_nfa.AddTransition(state4.ID(), transition43);
+
+    left_nfa.SetFinal(state2.ID());
+    right_nfa.SetFinal(state5.ID());
+
+    NFA merged = MergeNFA(left_nfa, right_nfa);
+
+    // Test transitions amount
+    assert(merged.GetAllTransitions().size() == 6);
+
+    // Test state amount
+    assert(merged.GetStates().size() == 5);
+
+    // Test final states amount
+    assert(merged.GetFinalStates().size() == 1);
+
+    // Test epsilon transition
+    assert(merged.GetAllTransitions()[state2.ID()][0].Input() == "");
+
+    return 0;
+  } catch (const std::exception& e) {
+    std::cout << "FAILED with error " << ' ' << e.what() << '\n';
+    return 1;
+  }
+}
