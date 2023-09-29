7d85f882b2a1265e50579c6aebd953db6c961b4b
eg0rmeister <karpuhin.egor@gmail.com>
2023-09-29 08:02:12 +0300

    fixed merge functions

--- tests/test_REGtoNFA.cpp
+++ tests/test_REGtoNFA.cpp
@@ -1,6 +1,6 @@
-#include <REG_to_NFA.h>
-#include <REGTree.h>
 #include <NFA.h>
+#include <REGTree.h>
+#include <REG_to_NFA.h>
 
 #include <cassert>
 #include <iostream>
@@ -11,10 +11,11 @@
 int main() {
   std::string regexp = "ab*+c";
   try {
-
     REGTree regtree(regexp);
     NFA nfa = GetNFAFromREG(regtree);
     nfa.Visualize();
+    std::cout << nfa.GetStartState().ID() << ' ' << nfa.GetFinalStates().size()
+              << std::endl;
     return 0;
   } catch (const std::exception& e) {
     std::cout << "FAILED with error " << ' ' << e.what() << '\n';
