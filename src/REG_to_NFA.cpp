2f2793ef4c3a7e56d027915be7674f78381ebf3a
eg0rmeister <karpuhin.egor@gmail.com>
2023-09-29 04:14:40 +0300

    added REG_to_NFA, not tested yet

--- src/REG_to_NFA.cpp
+++ src/REG_to_NFA.cpp
@@ -1,1 +1,30 @@
 #include <REG_to_NFA.h>
+
+NFA GetNFAFromNode(const REGTree::Node node) {
+  switch (node.GetType()) {
+    case REGTree::Concatenation:
+      return ConcatenateNFA(GetNFAFromNode(node.GetLeftChild()),
+                            GetNFAFromNode(node.GetRightChild()));
+      break;
+
+    case REGTree::LetterConstant:
+      return BuildOneLetterNFA(node.GetSymbol());
+      break;
+
+    case REGTree::Iteration:
+      return IterateNFA(GetNFAFromNode(node.GetLeftChild()));
+      break;
+
+    case REGTree::Addition:
+      return AddNFA(GetNFAFromNode(node.GetLeftChild()),
+                    GetNFAFromNode(node.GetRightChild()));
+      break;
+
+    default:
+      throw std::runtime_error("REGTree is invalid");
+  }
+}
+
+NFA GetNFAFromREG(const REGTree tree) {
+  return GetNFAFromNode(tree.GetRootNode());
+}
