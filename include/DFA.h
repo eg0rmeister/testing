7d3fff3099d140abbf445e77735bd0218aa14cb6
eg0rmeister <karpuhin.egor@gmail.com>
2023-09-30 14:03:54 +0300

    declared DFA movement

--- include/DFA.h
+++ include/DFA.h
@@ -7,4 +7,17 @@
 class DFA : public FSA {
  public:
   DFA(const State& start_state, const states_vec& states);
+
+  /// @brief Moves the state of the automaton according to the given letter
+  /// @param input Letter to move by
+  /// @return Whether the resulting state is final
+  bool Input(std::string input);
+
+  /// @brief Checks whether the DFA is in final state
+  /// @return true if the DFA is in final state
+  bool IsFinal() const;
+
+
+  /// @brief Returns automaton to starting state
+  void ResetState();
 };
