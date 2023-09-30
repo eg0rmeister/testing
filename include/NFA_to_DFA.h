8b583c25b07d6cc86abac6e5fcb6a99fd841b2f5
dm1trykrylov <231014d@gmail.com>
2023-09-30 22:36:49 +0300

    Remove hard-coded alphabet and add comments

--- include/NFA_to_DFA.h
+++ include/NFA_to_DFA.h
@@ -3,4 +3,7 @@
 #include <NFA.h>
 #include <DFA.h>
 
-DFA ConvertNFAtoDFA(NFA nfa);
+/// @brief Build equivalent DFA for given NFA using Thompson's algorithm
+/// @param nfa NFA without epsilon-transitions. Existence of eps-transitions causes UB.
+/// @return DFA accepting same language as original NFA.
+DFA ConvertNFAtoDFA(const NFA& nfa);
--- src/NFA_to_DFA.cpp
+++ src/NFA_to_DFA.cpp
@@ -5,10 +5,13 @@
 #include <unordered_set>
 #include <vector>
 
-DFA ConvertNFAtoDFA(NFA nfa) {
+DFA ConvertNFAtoDFA(const NFA& nfa) {
+  // Sets of NFA states are not explicitly stored in this implementation.
+  // Instead we use bitmasks to save memory and be able to use hash-based structures.
+  // For that purpose we enumerate all NFA states with numbers 0 ... nfa_states_count - 1.
+  // We use id_mapper to get index of particular state.
   auto nfa_states = nfa.GetStates();
   size_t nfa_states_count = nfa_states.size();
-  char alphabet[] = {'a', 'b', 'c', 'd'};
 
   using bitmask = std::vector<bool>;
   using triple = std::pair<State, Transition>;
@@ -35,32 +38,49 @@ DFA ConvertNFAtoDFA(NFA nfa) {
   std::unordered_map<std::vector<bool>, State> mask_to_state;
   mask_to_state[current_set] = start_state;
 
+  std::unordered_map<std::string, std::vector<Transition>> transition_by_letter;
   while (!queue.empty()) {
     current_set = queue.front().first;
     State src_state_dfa = queue.front().second;
     queue.pop();
-    for (auto c : alphabet) {
-      bitmask new_set(nfa_states_count, false);
-      
-      bool is_empty = true;
-      for (size_t i = 0; i < nfa_states_count; ++i) {
-        if (current_set[i]) {
-          auto new_transitions =
-              nfa.GetTransitionsByLetter(nfa_states[i], std::string(1, c));
-          for (auto& transition : new_transitions) {
-            is_empty = false;
-            new_set[id_mapper[transition.Target().ID()]] = true;
-          }
+    transition_by_letter.clear();
+
+    // Get all outgoing transitions from states in current_set
+    // and group them by letter
+    for (size_t i = 0; i < nfa_states_count; ++i) {
+      if (current_set[i]) {
+        auto new_transitions = nfa.GetTransitions(nfa_states[i]);
+        for (const auto& transition : new_transitions) {
+          transition_by_letter[transition.Input()].push_back(transition);
         }
       }
+    }
+
+    // Process transitions for each letter
+    for (const auto& transitions_group : transition_by_letter) {
+      std::string letter = transitions_group.first;
+
+      // Set of states reachable from current_states by this letter
+      bitmask new_set(nfa_states_count, false);
+
+      // Flag to avoig 'dead end' states
+      bool is_empty = true;
+      for (const auto& transition : transitions_group.second) {
+        is_empty = false;
+        // Add new state to set of reachable ones
+        new_set[id_mapper[transition.Target().ID()]] = true;
+      }
+
       State dest_state_dfa = mask_to_state[new_set];
       if (!is_empty) {
         dfa_transitions.push_back(
-            {src_state_dfa, Transition(std::string(1, c), dest_state_dfa)});
+            {src_state_dfa, Transition(letter, dest_state_dfa)});
 
         if (!dfa_states_masks.contains(new_set)) {
+          // Set of states in NFA is a new state in DFA
           dfa_states_masks.insert(new_set);
           bool is_final = false;
+          // Check if there is final state among reachable ones
           for (size_t i = 0; i < nfa_states_count; ++i) {
             if (new_set[i] && nfa.IsFinal(nfa_states[i].ID())) {
               is_final = true;
@@ -76,6 +96,8 @@ DFA ConvertNFAtoDFA(NFA nfa) {
       }
     }
   }
+
+  // Construct DFA
   DFA dfa(dfa_states[0], dfa_states);
   for (const auto& transition : dfa_transitions) {
     dfa.AddTransition(transition.first.ID(), transition.second);
