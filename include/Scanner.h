460794696bb0a2de4b951f7d27ea27384ebea450
eg0rmeister <karpuhin.egor@gmail.com>
2023-10-04 13:09:32 +0300

    declared Scanner and a few of its methods

---
+++ include/Scanner.h
@@ -0,0 +1,32 @@
+#pragma once
+#include "DFA_Forest.h"
+#include <string>
+
+class Scanner {
+ public:
+  /// @brief constructor of Scanner
+  /// @param forest forest
+  Scanner(DFAForest forest);
+
+  /// @brief Scan one symbol input
+  /// @param input symbol to scan
+  /// @return true if the state turned to ERROR
+  bool Input(std::string input);
+
+  /// @brief Resets scanner lexem
+  void Reset();
+
+  /// @brief Gets the id of the rule that accepted the last final state
+  /// @return id of the rule
+  uint32_t GetLastTerminalId() const;
+
+  /// @brief Gets the lexeme that was last accepted by a rule
+  /// @return the string corresponding to the lexeme
+  std::string GetLastTerminalString() const;
+
+ private:
+
+  std::string _current_lexem = "";
+  std::string _last_terminal_lexem = "";
+  uint32_t _last_terminal;
+};
