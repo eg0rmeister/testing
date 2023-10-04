9b81b5b74e392ad30fcb26d799eb68eed924e296
eg0rmeister <karpuhin.egor@gmail.com>
2023-10-04 13:33:29 +0300

    implemented Constructor and Input of Scanner

--- src/Scanner.cpp
+++ src/Scanner.cpp
@@ -1,3 +1,12 @@
 #include "Scanner.h"
 
-Scanner::Scanner(DFAForest forest) {}
+Scanner::Scanner(DFAForest forest) : _forest(forest) {}
+
+bool Scanner::Input(std::string input) {
+  _current_lexem += input;
+  if (_forest.Input(input)) {
+    _last_terminal = _forest.WhichFinal()[0];
+    _last_terminal_lexem = _current_lexem;
+  }
+  return _forest.IsError();
+}
