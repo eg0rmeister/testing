fed6456751e4d5e68ad592c5f8e09a4ae5e406ec
dm1trykrylov <231014d@gmail.com>
2023-10-13 19:18:36 +0300

    Add files

---
+++ CMakeFiles/demo.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/demo.dir/antlr4cpp_generated_src/TLexer/TLexer.cpp.o"
+  "CMakeFiles/demo.dir/antlr4cpp_generated_src/TLexer/TLexer.cpp.o.d"
+  "CMakeFiles/demo.dir/antlr4cpp_generated_src/TParser/TParser.cpp.o"
+  "CMakeFiles/demo.dir/antlr4cpp_generated_src/TParser/TParser.cpp.o.d"
+  "CMakeFiles/demo.dir/main.cpp.o"
+  "CMakeFiles/demo.dir/main.cpp.o.d"
+  "antlr4cpp_generated_src/TLexer/TLexer.cpp"
+  "antlr4cpp_generated_src/TLexer/TLexer.h"
+  "antlr4cpp_generated_src/TLexer/TLexer.interp"
+  "antlr4cpp_generated_src/TLexer/TLexer.tokens"
+  "antlr4cpp_generated_src/TParser/TParser.cpp"
+  "antlr4cpp_generated_src/TParser/TParser.h"
+  "antlr4cpp_generated_src/TParser/TParser.interp"
+  "antlr4cpp_generated_src/TParser/TParser.tokens"
+  "demo"
+  "demo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
