34d17f24e0c1b5a3e098947c83cc24b8d3604df8
dm1trykrylov <231014d@gmail.com>
2023-11-03 19:17:17 +0400

    Change tag to last stable release to develop offline

--- cmake/ExternalAntlr4Cpp.cmake
+++ cmake/ExternalAntlr4Cpp.cmake
@@ -12,7 +12,7 @@ set(ANTLR4_GIT_REPOSITORY https://github.com/antlr/antlr4.git)
 if(NOT DEFINED ANTLR4_TAG)
   # Set to branch name to keep library updated at the cost of needing to rebuild after 'clean'
   # Set to commit hash to keep the build stable and does not need to rebuild after 'clean'
-  set(ANTLR4_TAG master)
+  set(ANTLR4_TAG 5a7b4d4)
 endif()
 
 # Ensure that the include dir already exists at configure time (to avoid cmake erroring
