ddbd07c41e32e8290f62da702d7178c36b2e779c
eg0rmeister <karpuhin.egor@gmail.com>
2023-11-17 14:09:43 +0300

    implemented stack memory class

---
+++ Include/StackMemory.h
@@ -0,0 +1,62 @@
+#pragma once
+#include <map>
+#include <stack>
+#include <string>
+#include <vector>
+
+class StackMemory {
+  void Declare(const std::string& name, int value) {
+    if (CheckDeclared(name)) {
+      throw(std::runtime_error("Variable " + name + " already declared in this scope"));
+    }
+    _variables_history[name].push(value);
+  }
+
+  void Set(const std::string& name, int value) {
+    if (!CheckDeclared(name)) {
+      throw(std::runtime_error("Variable " + name + " was not declared in this scope"));
+    }
+    _variables_history[name].pop();
+    _variables_history[name].push(value);
+  }
+  
+  int Get(const std::string& name, int value) {
+    if (!CheckDeclared(name)) {
+      throw(std::runtime_error("Variable " + name + " was not declared in this scope"));
+    }
+    return _variables_history.at(name).top();
+  }
+
+  void Scope_in() {
+    _declare_history.push_back(_declare_delimiter);
+  }
+
+  void Scope_out() {
+    std::string current_name = _declare_history.back();
+    _declare_history.pop_back();
+    while (current_name != _declare_delimiter) {
+      _variables_history.at(current_name).pop();
+      current_name = _declare_history.back();
+      _declare_history.pop_back();
+    }
+  }
+
+ private:
+  bool CheckDeclared(const std::string& name) {
+    size_t distance_from_end = 0;
+    while (distance_from_end < _declare_history.size()) {
+      if (_declare_history[_declare_history.size() - distance_from_end] == _declare_delimiter) {
+        break;
+      }
+      if (_declare_history[_declare_history.size() - distance_from_end] == name) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  std::string _declare_delimiter = "{";
+
+  std::vector<std::string> _declare_history;
+  std::map<std::string, std::stack<int>> _variables_history;
+};
