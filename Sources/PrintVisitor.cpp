ba92b2dc6e35331f7123b714ff4d620a07d9d9cf
eg0rmeister <karpuhin.egor@gmail.com>
2023-11-22 12:21:11 +0300

    added VisitFile definition

--- Sources/PrintVisitor.cpp
+++ Sources/PrintVisitor.cpp
@@ -1,5 +1,12 @@
 #include <string>
+#include <iostream>
+#include "PrintVisitor.h"
 
-#include "antlr4-runtime.h"
-#include "ExprBaseVisitor.h"
-
+std::any PrintVisitor::visitFile(ExprParser::FileContext* context) {
+  std::cout << "File: " << context->getText() << std::endl;
+  for (auto function : context->fun()) {
+    function->accept(this);
+  }
+  context->prog()->accept(this);
+  return std::any();
+}
