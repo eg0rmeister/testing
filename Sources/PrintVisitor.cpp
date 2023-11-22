b9b61e0d2f4c46300d035d669b49b9cbce9a1e97
eg0rmeister <karpuhin.egor@gmail.com>
2023-11-22 12:22:42 +0300

    Added visitProg and changed visitFile cout

--- Sources/PrintVisitor.cpp
+++ Sources/PrintVisitor.cpp
@@ -3,10 +3,18 @@
 #include "PrintVisitor.h"
 
 std::any PrintVisitor::visitFile(ExprParser::FileContext* context) {
-  std::cout << "File: " << context->getText() << std::endl;
+  std::cout << "File" << std::endl;
   for (auto function : context->fun()) {
     function->accept(this);
   }
   context->prog()->accept(this);
+  return std::any();
+}
+
+std::any PrintVisitor::visitProg(ExprParser::ProgContext* ctx) {
+  std::cout << "Main" << std::endl;
+  for (auto statement : ctx->stmt()) {
+    statement->accept(this);
+  }
   return std::any();
 }
