3f7ff76f4f7a9348c86d3f361fde614e66d7a86d
dm1trykrylov <231014d@gmail.com>
2023-12-01 21:59:42 +0300

    function add from seminar

--- example_short.cpp
+++ example_short.cpp
@@ -1,52 +1,57 @@
-#include <llvm/IR/LLVMContext.h>
-#include <llvm/IR/Module.h>
-#include <llvm/IR/Function.h>
-#include <llvm/IR/IRBuilder.h>
-#include <llvm/IR/Type.h>
-#include <llvm/IR/Constants.h>
-#include <iostream>
 #include <fstream>
+#include <iostream>
+
+#include "llvm/ADT/APFloat.h"
+#include "llvm/ADT/STLExtras.h"
+#include "llvm/IR/BasicBlock.h"
+#include "llvm/IR/Constants.h"
+#include "llvm/IR/DerivedTypes.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/LLVMContext.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IR/Type.h"
+#include "llvm/IR/Verifier.h"
 
 int main() {
-    llvm::LLVMContext context;
-    llvm::Module module("llvm_example", context);
-    llvm::IRBuilder<> builder(context);
+  llvm::LLVMContext context_;
+  llvm::Module module_("llvm_example", context_);
+  llvm::IRBuilder<> builder_(context_);
 
-    // Define the printf function signature
-    std::vector<llvm::Type *> printfArgs;
-    printfArgs.push_back(builder.getInt8Ty()->get(context, 8)); // Use i8* instead of ptr
-    printfArgs.push_back(builder.getInt32Ty());
-    auto printfType = llvm::FunctionType::get(builder.getInt32Ty(), printfArgs, true);
-    auto *printfFunc = llvm::Function::Create(printfType, llvm::Function::ExternalLinkage, "printf", &module);
+  auto function_type =
+      llvm::FunctionType::get(llvm::Type::getVoidTy(context_), false);
 
-    // Define the main function
-    auto mainFuncType = llvm::FunctionType::get(builder.getInt32Ty(), false);
-    auto *mainFunc = llvm::Function::Create(mainFuncType, llvm::Function::ExternalLinkage, "main", &module);
-    auto *mainBlock = llvm::BasicBlock::Create(context, "entry", mainFunc);
-    builder.SetInsertPoint(mainBlock);
+  // Create main function
+  auto function_llvm = llvm::Function::Create(
+      function_type, llvm::Function::ExternalLinkage, "main", module_);
 
-    // Call printf with "Hello world"
-    auto *formatStr = builder.CreateGlobalString("Hello world\n");
-    std::vector<llvm::Value *> printfArgsV;
-    printfArgsV.push_back(formatStr);
-    printfArgsV.push_back(builder.getInt32(0)); // Dummy argument for "%d" in printf format
-    builder.CreateCall(printfFunc, printfArgsV, "printfCall");
+  auto entry = llvm::BasicBlock::Create(context_, "entry", function_llvm);
 
-    // Return 0 from main
-    builder.CreateRet(builder.getInt32(0));
+  builder_.SetInsertPoint(entry);
 
-    // Save LLVM IR to a file
-    std::error_code ec;
-    llvm::raw_fd_ostream outputFile("llvm_example.ll", ec, llvm::sys::fs::OpenFlags());
+  auto add_function_type = llvm::FunctionType::get(
+      llvm::Type::getInt32Ty(context_),
+      {builder_.getInt32Ty(), builder_.getInt32Ty()}, false);
+  
+  //auto add_function = llvm::Function::Create(
+  //    add_function_type, llvm::Function::ExternalLinkage, "add", module_);
+  
+  // builder_.CreateRet(llvm::ConstantInt::get(context_, llvm::APInt()));
+  
+  // Save LLVM IR to a file
+  std::error_code ec;
+  llvm::raw_fd_ostream outputFile("llvm_example.ll", ec,
+                                  llvm::sys::fs::OpenFlags());
 
-    if (!ec) {
-        module.print(outputFile, nullptr);
-        outputFile.flush();
-        outputFile.close();
-        std::cout << "LLVM IR written to llvm_example.ll" << std::endl;
-    } else {
-        std::cerr << "Error writing to llvm_example.ll: " << ec.message() << std::endl;
-    }
+  if (!ec) {
+    module_.print(outputFile, nullptr);
+    outputFile.flush();
+    outputFile.close();
+    std::cout << "LLVM IR written to llvm_example.ll" << std::endl;
+  } else {
+    std::cerr << "Error writing to llvm_example.ll: " << ec.message()
+              << std::endl;
+  }
 
-    return 0;
+  return 0;
 }
