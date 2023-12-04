390226ff08790e764a84ec64df06e4c31e41bc60
eg0rmeister <karpukhin.ea@phystech.edu>
2023-12-05 00:24:57 +0300

    changed driver to only include irvisitor

--- driver.cpp
+++ driver.cpp
@@ -19,15 +19,22 @@ int main(int argc, const char* argv[]) {
 
   InterpreterVisitor visitor;
   PrintVisitor print_visitor;
+  IRVisitor ir_visitor;
+  // try {
+  //   print_visitor.visitFile(tree);
+  // } catch (const std::exception& ex) {
+  //   std::cout << "Error occured in print pass!\n" << ex.what() << std::endl;
+  // }
+  // try {
+  //   visitor.visitFile(tree);
+  // } catch (const std::exception& ex) {
+  //   std::cout << "Error occured in interpret pass!\n" << ex.what() << std::endl;
+  // }
   try {
-    print_visitor.visitFile(tree);
+    ir_visitor.visitFile(tree);
+    ir_visitor.printIR();
   } catch (const std::exception& ex) {
-    std::cout << "Error occured!\n" << ex.what() << std::endl;
-  }
-  try {
-    visitor.visitFile(tree);
-  } catch (const std::exception& ex) {
-    std::cout << "Error occured!\n" << ex.what() << std::endl;
+    std::cout << "Error occured in translation pass!\n" << ex.what() << std::endl;
   }
 
   return 0;
